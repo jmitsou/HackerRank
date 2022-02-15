cea16fd03bc8b39428c60016f338aeef74235bf7
Jeff Mitsou <shinetachi88@gmail.com>
2022-02-15 17:52:53 -0500

    updated Algorithms

--- src/com/company/TeamCoding/Jumping_Clouds.java
+++ src/com/company/TeamCoding/Jumping_Clouds.java
@@ -1,4 +1,15 @@
 package com.company.TeamCoding;
 
+//create HashMap with 2 keys
+//create variable for energy lvl set to 100
+//loop through int array by var k (jump length) and minus energy by 1 for clouds and 2 for thunderclouds
+
 public class Jumping_Clouds {
+    int energyLvl = 100;
+
+    public int jumpingOnClouds(int[] c, int k) {
+
+
+       return energyLvl;
+    }
 }
--- src/com/company/TeamCoding/Sherlock_N_Anagrams.java
+++ src/com/company/TeamCoding/Sherlock_N_Anagrams.java
@@ -1,4 +1,18 @@
 package com.company.TeamCoding;
 
+//convert q into a list
+//loop through list
+//place into hashMap and check for repeats
+
+
 public class Sherlock_N_Anagrams {
+    String s = "";
+    int sCount = 0;
+
+    public int sherlockAndAnagrams(String s) {
+
+
+        return sCount;
+    }
+
 }
