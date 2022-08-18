9a00e3af636d96fcc253b9bf277df91977a0b492
Jeff Mitsou <shinetachi88@gmail.com>
2022-08-18 16:26:44 -0400

    Completed Day 11: 2D Arrays

--- src/com/company/ThirtyDayOfCode/Day11.java
+++ src/com/company/ThirtyDayOfCode/Day11.java
@@ -32,23 +32,30 @@ public class Day11 {
         int row = 0;
         int column = 0;
         int sum = 0;
+        int maxSum=Integer.MIN_VALUE;
+        int hourGlasses = 16;
+
         //find the all of hourglasses
-        for (int i = 0; i < arr.get(row).size(); i++) {
-            int pos = 0;
-            //when pos reaches 3 reset sum
-            if (pos == 3 ){
+        for (int i = 0; i < hourGlasses; i++) {
 
+            //find the sum of each of the hour glasses
+            sum = arr.get(row).get(column) + arr.get(row).get(column+1) + arr.get(row).get(column+2) + arr.get(row+1).get(column+1)+arr.get(row+2).get(column) + arr.get(row+2).get(column+1) + arr.get(row+2).get(column+2);
+            column++;
+            //compare all the sums
+            if(sum > maxSum){
+                maxSum= sum;
             }
-            //for (int j = 1; j < arr.get(row).get(column).size()-1; j++) {
+            //System.out.println(sum);
+            //when pos reaches 3 reset sum
+            if (column == 4 ){
+                row++;
+                column =0;
+            }
 
-            //}
         }
+        //System.out.println(arr.size());
 
-        //sum = arr.get(row).get(column) + arr.get(row).get(column+1) + arr.get(row).get(column+2) + arr.get(row+1).get(column+1)+arr.get(row+2).get(column) + arr.get(row+2).get(column+1) + arr.get(row+2).get(column+2);
-
-        //find the sum of each of the hour glasses
-
-        //compare all the sums and print out the largest
-        System.out.println(sum);
+        //print out the largest
+        System.out.println(maxSum);
     }
 }
